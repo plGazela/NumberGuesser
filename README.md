f42f3484dbaf9c15736b7d642dc2297ad8e774bd
Jakub Gezela <pl.jgezela@gmail.com>
2023-04-23 22:20:36 +0200

    Code cleanup
    Changed some code to be more esthetic and more readable. Added game manual to README file.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # 🔢 NumberGuesser
 A simple number-guessing game in your browser.
+
+## How does it work?
+It's a simple game that will draw a number to guess, a player has 3 lives at the start and with every correct answer, the player gets additional life and point. When a player is wrong the life is taken but the number to guess stays the same. The game ends when a player is out of all lives.
+
+There are 3 difficulty levels, and with 5 correct answers, a player will get to the next level. A player's goal is to get as many points as he can.
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,115 +1,6 @@
-// TOOD: Delete repeating numbers
-
-// Game init
-function gameStart() {
-    // UI elements
-
-    // Text and input
-    const bodyEl = document.getElementsByClassName("game")[0];
-    const taskEl = document.getElementsByClassName("game__task")[0];
-    const taskPromptEl = document.getElementsByClassName("game__prompt__question")[0];
-    const pointsEl = document.getElementsByClassName("game__points")[0];
-    const answerIn = document.getElementsByClassName("game__answer")[0];
-
-    // Buttons
-    const submitBtn = document.getElementsByClassName("game__answer-submit")[0];
-    const restartBtn = document.getElementsByClassName("game__restart")[0];
-
-    // Variables
-    let gamedrawnNumbers = [];
-    let gameDifficultyLevel = 0;
-    let gamePlayerLives = 3;
-    let gamePlayerPoints = 0;
-    let gameDrawnNumber = drawNumber(gameDifficultyLevel);
-
-    gamedrawnNumbers.push(gameDrawnNumber);
-
-    answerIn.focus()
-
-    // DEBUG
-    console.log("Drawn number: ", gameDrawnNumber);
-
-    // Player submit answer
-    submitBtn.addEventListener("click", () => {
-        if(checkGuess(gameDrawnNumber)) {
-            // Correct answer
-            gamePlayerLives++;
-            gamePlayerPoints++;
-            pointsEl.textContent = gamePlayerPoints;
-            answerIn.value = null;
-            answerIn.focus();
-
-            correctAnswerEffect();
-
-            // DEBUG
-            console.log("Correct answer! Lives: " + gamePlayerLives);
-
-            // Changing difficulty level based on player points
-            if(gamePlayerPoints === 5) {
-                gameDifficultyLevel++;
-                taskPromptEl.textContent = "1-30";
-
-                // DEBUG
-                console.log("Difficulty level up!");
-            } else if(gamePlayerPoints === 10) {
-                gameDifficultyLevel++;
-                taskPromptEl.textContent = "1-50";
-
-                // DEBUG
-                console.log("Difficulty level up!");
-            } else if(gamePlayerPoints === 15){
-                gameDifficultyLevel++;
-                taskPromptEl.textContent = "1-100";
-
-                // DEBUG
-                console.log("Difficulty level up!");
-            }
-
-            // Draw new number
-            gameDrawnNumber = drawNumber(gameDifficultyLevel);
-            while(checkNumber(gameDrawnNumber, gamedrawnNumbers) == true) {
-                gameDrawnNumber = drawNumber(gameDifficultyLevel);
-            }
-            gamedrawnNumbers.push(gameDrawnNumber);
-
-            // DEBUG
-            console.log("Drawn number: ", gameDrawnNumber);
-
-        } else {
-            // Wrong answer
-            gamePlayerLives--;
-            wrongAnswerEffect();
-            answerIn.value = null;
-            answerIn.focus();
-
-            // DEBUG
-            console.log("Wrong answer! Lives: " + gamePlayerLives);
-
-            if(gamePlayerLives === 0) {
-                console.log("Game Over!");
-
-                setTimeout(() => {
-                    bodyEl.style.animation = "none";
-                }, 10);
-                bodyEl.style.background = "var(--background-wrong-answer)";
-                taskEl.textContent = "Game Over!";
-                taskPromptEl.style.display = "none";
-                answerIn.style.display = "none";
-                submitBtn.style.display = "none";
-                restartBtn.style.display = "block";
-            } 
-        }
-    });
-
-    // Player restart game
-    restartBtn.addEventListener("click", () => {
-        location.reload();
-    })
-}
-gameStart();
-
-// Drawing a random number based on difficulty level
+// Drawing a random number based on the difficulty level
 function drawNumber(difficultyLevel) {
+
     let difficultyFactor;
 
     switch(difficultyLevel) {
@@ -128,12 +19,12 @@ function drawNumber(difficultyLevel) {
     }
 
     let drawnNumber = Math.floor(Math.random() * difficultyFactor) + 1;
-    
     return drawnNumber;
 }
 
 // Checking player answers
 function checkGuess(drawnNumber) {
+
     let playerAnswer = Number(document.getElementsByClassName("game__answer")[0].value);
     
     if(playerAnswer === drawnNumber) {
@@ -145,9 +36,9 @@ function checkGuess(drawnNumber) {
 
 // Checking number repeatability
 function checkNumber(drawnNumber, ...drawnNumbers) {
+
     for(let i=0; i<drawnNumbers[0].length; i++) {
         if(drawnNumber === drawnNumbers[0][i]) {
-
             return true;
         }
     }
@@ -155,21 +46,111 @@ function checkNumber(drawnNumber, ...drawnNumbers) {
     return false;
 }
 
-// Correct and wrong answer broswer effect
+// Correct and wrong answer effect
 function correctAnswerEffect() {
     const gameBody = document.getElementsByClassName("game")[0];
 
     gameBody.style.animation = "none";
     setTimeout(() => {
-        gameBody.style.animation = "correct-answer-animation 1s forwards";
+        gameBody.style.animation = "correct-answer 1s forwards";
     }, 10);
 }
-
 function wrongAnswerEffect() {
     const gameBody = document.getElementsByClassName("game")[0];
 
     gameBody.style.animation = "none";
     setTimeout(() => {
-        gameBody.style.animation = "wrong-answer-animation 1s forwards";
+        gameBody.style.animation = "wrong-answer 1s forwards";
     }, 10);
 }
+
+// Game init
+function gameStart() {
+
+    // Elements and input
+    const bodyEl = document.getElementsByClassName("game")[0];
+    const pointsEl = document.getElementsByClassName("game__points")[0];
+    const taskEl = document.getElementsByClassName("game__task")[0];
+    const taskPromptEl = document.getElementsByClassName("game__prompt__question")[0];
+    const answerIn = document.getElementsByClassName("game__answer")[0];
+
+    // Buttons
+    const submitBtn = document.getElementsByClassName("game__submit")[0];
+    const restartBtn = document.getElementsByClassName("game__restart")[0];
+
+    // Variables
+    let gamedrawnNumbers = [];
+    let gameDifficultyLevel = 0;
+    let gamePlayerLives = 3;
+    let gamePlayerPoints = 0;
+    let gameDrawnNumber = drawNumber(gameDifficultyLevel);
+
+    // Pushing first number to check and focusing input element
+    gamedrawnNumbers.push(gameDrawnNumber);
+    answerIn.focus()
+
+    // Player submit answer
+    submitBtn.addEventListener("click", () => {
+        if(checkGuess(gameDrawnNumber)) {
+
+            // Player correct answer (adding lives, points, running effect, etc.)
+            gamePlayerLives++;
+            gamePlayerPoints++;
+            pointsEl.textContent = gamePlayerPoints;
+            answerIn.value = null;
+            answerIn.focus();
+            correctAnswerEffect();
+
+            // Changing difficulty level based on player points
+            if(gamePlayerPoints === 5) {
+                gameDifficultyLevel++;
+                taskPromptEl.textContent = "1-30";
+
+            } else if(gamePlayerPoints === 10) {
+                gameDifficultyLevel++;
+                taskPromptEl.textContent = "1-50";
+
+            } else if(gamePlayerPoints === 15){
+                gameDifficultyLevel++;
+                taskPromptEl.textContent = "1-100";
+
+            }
+
+            // Draw a new number and check if it's not repeating
+            gameDrawnNumber = drawNumber(gameDifficultyLevel);
+            while(checkNumber(gameDrawnNumber, gamedrawnNumbers) == true) {
+                gameDrawnNumber = drawNumber(gameDifficultyLevel);
+            }
+            gamedrawnNumbers.push(gameDrawnNumber);
+
+        } else {
+
+            // Player wrong answer (losing lives, running effect, etc.)
+            gamePlayerLives--;
+            wrongAnswerEffect();
+            answerIn.value = null;
+            answerIn.focus();
+
+            // Player run out of lives (change game screen)
+            if(gamePlayerLives === 0) {
+                setTimeout(() => {
+                    bodyEl.style.animation = "none";
+                }, 10);
+                bodyEl.style.background = "var(--background-wrong-answer)";
+                taskEl.textContent = "Game Over!";
+                taskPromptEl.style.display = "none";
+                answerIn.style.display = "none";
+                submitBtn.style.display = "none";
+                restartBtn.style.display = "block";
+            } 
+        }
+    });
+
+    // Player restarting game
+    restartBtn.addEventListener("click", () => {
+        location.reload();
+    })
+}
+
+// Start game
+gameStart();
